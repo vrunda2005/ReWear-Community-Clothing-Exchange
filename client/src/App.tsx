b2b594330458415b08cea9cce4e3249e89d5d298
Devashree <devashree.p@ahduni.edu.in>
2025-07-12 09:12:07 +0000

    ui done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signup from './components/auth/Signup';
 import Login from './components/auth/Login';
 import Dashboard from './pages/Dashboard';
+import LandingPage from './pages/Landingpage';
+import SwapSwiper from './pages/swapswiper';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/swap" element={<SwapSwiper />} />
       </Routes>
     </Router>
   );
